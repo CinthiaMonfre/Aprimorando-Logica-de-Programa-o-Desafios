71b0a312fe9f9e51e6f16eab0809910c310dde41
CinthiaMonfre <cinthia.monfre@gmail.com>
2023-11-21 16:47:04 -0300

    inclusão terceiro cod

---
+++ Explorando Condicionais JavaScript.js
@@ -0,0 +1,22 @@
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+// Entrada de dados. Lembre-se: O parseInt(()) é importante para a conversão dos valores de entrada(String) para um valor numérico(int).
+let escolhaDoTreinador = parseInt(gets());
+let pokemonEscolhido; 
+
+if ( escolhaDoTreinador === 1) {
+    pokemonEscolhido = "Bulbasaur";
+} else if ( escolhaDoTreinador === 2 ) {
+    pokemonEscolhido = "Charmander";
+} else if ( escolhaDoTreinador === 4 ) {
+    pokemonEscolhido = "Pikachu";
+} else{
+    pokemonEscolhido = "Mewtwo";
+}
+
+//Imprime o Pokémon escolhido:
+if (pokemonEscolhido) {
+    print("Voce escolheu o " + pokemonEscolhido + " como seu Pokemon inicial.");
+}
